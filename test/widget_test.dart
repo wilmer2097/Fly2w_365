fe7cc1afdd6eef18a15c797a429f28f557fcb497
Wilmer <wilmer20acero@gmail.com>
2025-05-19 15:27:51 -0700

    cambios ios

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fly2w_365/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(FLY2WApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
